d8822fcc37875d33668ee721a81cdea2679f6eec
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-05-15 10:09:45 +1000

    Changes made

--- censor-dispensorv2.py
+++ censor-dispensorv2.py
@@ -8,11 +8,14 @@ proprietary_terms = ["personality matrix",  "learning algorithm", "learning algo
 negative_words = ["concerned", "out of control", "behind", "dangerous", "danger", "alarming", "alarmed", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressing", "concerning", "horrible", "horribly", "questionable"]
 list_of_words_to_censor = negative_words + proprietary_terms
 
-email_four_lower = email_four.lower()
+email_four2 = email_four
+email_four_lower = email_four2.lower()
 for word in list_of_words_to_censor:
+    print(email_four_lower)
+    print(word)
     if word in email_four_lower:
-        index_of_word = email_four.index(word)
-        while word in email_four:
+        while word in email_four2:
+            index_of_word = email_four_lower.index(word)
             censored_word = ""
             for i in range(len(word)):
                 if word[i] == " ":
@@ -20,10 +23,9 @@ for word in list_of_words_to_censor:
                 else:
                     censored_word += "#" 
             email_four_lower = email_four_lower.replace(word, censored_word)
-            email_four = email_four.replace(word, censored_word)
-email_word_list = [word for word in email_four.split(" ")]
+            email_four2 = email_four2.replace(word, censored_word)
+email_word_list = [word for word in email_four2.split(" ")]
 idx = 0
-print(email_four)
 for word in email_word_list:
     if '#' in word:
         pre_word = email_word_list[idx-1]
@@ -34,18 +36,18 @@ for word in email_word_list:
                 censored_word += " "
             else:
                 censored_word += "#" 
-        email_four = email_four.replace(pre_word, censored_word)
+        email_four2 = email_four2.replace(pre_word, censored_word)
+        censored_word = ""
         for i in range(len(post_word)):
             if post_word[i] == " ":
                 censored_word += " "
             else:
                 censored_word += "#" 
-        email_four = email_four.replace(post_word, censored_word)
-        idx+=1
+        email_four2 = email_four2.replace(post_word, censored_word)
     idx+=1
 
 
-print(email_four)
+print(email_four2)
 
 
 
